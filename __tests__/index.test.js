d2d9522049648cb72cfe2a1badfc369b61ed8aa3
Sergei Shinkarenko <serewja@gmail.com>
2020-07-13 16:30:13 +0500

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,9 @@
-import f from "../dist/index.js"
+import addDigits from "../dist/index.js"
 
-test('testname', () => {
-    
+test('test', () => {
+  expect(addDigits(10)).toBe(1);
+  expect(addDigits(19)).toBe(1);
+  expect(addDigits(10)).toBe(1);
+  expect(addDigits(38)).toBe(2);
+  expect(addDigits(1259)).toBe(8);
 });
